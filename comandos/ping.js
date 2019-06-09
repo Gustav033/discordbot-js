90c8c620d7c25f978a896fea28b3294faa74e1bd
Márcio DEV <31147955+DMVMarcio@users.noreply.github.com>
2019-06-08 22:56:20 -0300

    Update ping.js

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
 
-message.channel.sendMessage(`:ping_pong: Pong: **${parseInt(client.ping)}**ms`);
+  message.channel.send(`:ping_pong: Pong: **${parseInt(client.ping)}**ms`);
 
 }
