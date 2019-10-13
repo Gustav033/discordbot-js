e66f247246bb8641f81fec51542da057f3e55a0f
Gustav033 <55931689+Gustav033@users.noreply.github.com>
2019-10-13 16:15:00 -0300

    Update Ping.js - Por Gustavo33

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,5 +1,7 @@
 exports.run = ({client, message, args}) => {
-
-  message.channel.send(`:ping_pong: Pong: **${parseInt(client.ping)}**ms`);
-
+  let medidor = await message.channel.send("ping?");
+  let userms = medidor.createTimestamp - message.createdTimedtamp + "ms";
+  let pingfinal = `Pong! :ping_pong: | Ping: ${userms} | Ping Da Api: ${Math.floor(client.ping))`;
+  medidor.delete();
+  message.reply(pingfinal);
 }
