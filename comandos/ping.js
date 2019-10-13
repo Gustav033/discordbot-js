ba27a89287f371a09ebc3852f32cf2e0a8614fa2
Gustav033 <55931689+Gustav033@users.noreply.github.com>
2019-10-13 16:17:36 -0300

    Update Ping.js

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,5 +1,6 @@
-exports.run = ({client, message, args}) => {
 
-  message.channel.send(`:ping_pong: Pong: **${parseInt(client.ping)}**ms`);
-
+exports.run = async (client, message) => {
+    const ms = await message.channel.send("Ping?");
+	const clientms = ms.createdTimestamp - message.createdTimestamp;
+	ms.edit('📡 Seu ping é: ' + clientms + 'ms / 🖥 Ping do bot com Server:' + Math.round(client.ping) + 'ms');
 }
